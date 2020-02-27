7d469f3dc1420514c625b72c6d2985948a3b1c6d
Akshat Zala <akshatzala@gmail.com>
2020-02-27 22:07:41 +0530

    Added layout / views/ -2 buttons xml onclick and java onclick

--- app/src/main/java/com/example/layout_views/MainActivity.java
+++ app/src/main/java/com/example/layout_views/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.layout_views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView t1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t1 = findViewById(R.id.t1);
+        b1 = findViewById(R.id.b1);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        t1.setText("You have clicked on Button 1");
+    }
+
+    public void doSomething(View v) {
+        t1.setText("Your Text");
     }
 }
